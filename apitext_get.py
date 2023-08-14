dd8ba4859f2426eb033f88fd367eb5a4812fd31e
nobody1121 <1097393074@qq.com>
2023-08-14 15:48:04 +0800

    +没有参数时的逻辑判断

--- apitext_get.py
+++ apitext_get.py
@@ -19,7 +19,8 @@ def extract_request_details_and_send(file_path):
             key, value = line.split(': ', 1)
             original_headers[key] = value
 
-        scenarios(request_method, base_url, original_params, original_headers)
+        if original_params:
+            scenarios(request_method, base_url, original_params, original_headers)
 
         # 测试 Authorization 或 token 头部为空的场景
         if 'Authorization' in original_headers or 'token' in original_headers:
@@ -54,16 +55,12 @@ def scenarios(request_method, base_url, original_params, original_headers):
 
 def send_request(request_method, base_url, params, headers):
     params = {k: v[0] for k, v in params.items()}
-    # print(f"请求方法: {request_method}")
-    # print(f"基础URL: {base_url}")
-    # print(f"头部: {headers}")
     print(f"参数: {params}")
     response = requests.request(request_method, base_url, params=params, headers=headers)
     print(f"请求响应状态码: {response.status_code}")
     print(f"请求响应内容: {response.text}")
     print(f"请求响应耗时: {response.elapsed.total_seconds()} 秒")
     print("="*40)  # 分隔线
-
 
 # 获取当前目录下所有 .txt 文件
 file_paths = glob.glob('./apitext_get/*.txt')
--- apitext_post_urlencoded.py
+++ apitext_post_urlencoded.py
@@ -24,7 +24,19 @@ def extract_request_details_and_send(file_path):
         request_body = file_content[request_body_start + 2:request_body_end].strip()
 
         # 解析请求体
-        original_params = dict(parse_qsl(request_body))
+        original_params = dict(parse_qsl(request_body)) if request_body else {}
+
+        # 测试 Authorization 或 token 头部为空的场景
+        headers_copy = headers.copy()
+        if 'Authorization' in headers_copy:
+            headers_copy['Authorization'] = ''
+        if 'token' in headers_copy:
+            headers_copy['token'] = ''
+        send_request(request_method, url, original_params, headers_copy)
+
+        # 如果没有参数，结束此次测试
+        if not original_params:
+            return
 
         # 测试每个参数被留空的场景
         for key in original_params.keys():
@@ -45,24 +57,14 @@ def extract_request_details_and_send(file_path):
                 params[key] = '999999999'
                 send_request(request_method, url, params, headers)
 
-        # 测试 Authorization 或 token 头部为空的场景
-        if 'Authorization' in headers or 'token' in headers:
-            headers_copy = headers.copy()
-            if 'Authorization' in headers_copy:
-                headers_copy['Authorization'] = ''
-            if 'token' in headers_copy:
-                headers_copy['token'] = ''
-            send_request(request_method, url, original_params, headers_copy)
-
         # 测试时间盲注的场景
         params = original_params.copy()
-        last_key = list(original_params.keys())[-1]  # 获取最后一个参数的键
-        params[last_key] = original_params[last_key] + " OR IF(1=1, SLEEP(5), 0)"  # 修改最后一个参数的值
+        last_key = list(original_params.keys())[-1]
+        params[last_key] = original_params[last_key] + " OR IF(1=1, SLEEP(5), 0)"
         send_request(request_method, url, params, headers)
 
 
 def send_request(request_method, url, params, headers):
-    # print(f"头部: {headers}")
     print(f"参数: {params}")
     request_data = '&'.join([f"{key}={value}" for key, value in params.items()])
     response = requests.request(request_method, url, data=request_data, headers=headers)
@@ -70,7 +72,6 @@ def send_request(request_method, url, params, headers):
     print(f"请求响应内容: {response.text}")
     print(f"请求响应耗时: {response.elapsed.total_seconds()} 秒")
     print("=" * 40)  # 分隔线
-
 
 # 获取当前目录下所有 .txt 文件
 file_paths = glob.glob('./apitext_post_urlencoded/*.txt')
