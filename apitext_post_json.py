dcee3797891d4968f11beab82255369fa87b5533
nobody1121 <1097393074@qq.com>
2023-09-08 16:45:54 +0800

    post+content-type是json时，url带参数

--- apitext_post_json.py
+++ apitext_post_json.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from urllib.parse import urlparse, parse_qs
 import glob
 
 
@@ -20,56 +21,115 @@ def extract_request_details_and_send(file_path):
                 key = key.strip()  # 剔除头部名称中的多余空格和冒号
                 headers[key] = value.strip()
 
-        # 提取并解析请求体
-        request_body_start = file_content.index("{")
-        request_body_end = file_content.index("\nHTTP/1.1 200")  # 假设响应始终以"HTTP/1.1 200"开始
-        request_body = file_content[request_body_start:request_body_end].strip()
-        original_params = json.loads(request_body)
+        if "?" in url:
+            parsed_url = urlparse(url)
+            base_url = url.split('?')[0]
+            original_params_list = parse_qs(parsed_url.query)
+            original_params_url = {k: v[0] for k, v in original_params_list.items()}
 
-        # 测试每个参数被留空的场景
-        for key in original_params.keys():
-            params = original_params.copy()
-            params[key] = None if isinstance(params[key], int) else ''
-            print("测试场景：验证参数值必填项")
-            send_request(request_method, url, params, headers)
+            # 提取并解析请求体
+            request_body_start = file_content.index("{")
+            request_body_end = file_content.index("\nHTTP/1.1 200")  # 假设响应始终以"HTTP/1.1 200"开始
+            request_body = file_content[request_body_start:request_body_end].strip()
+            original_params_body = json.loads(request_body)
+            original_params = {**original_params_url, **original_params_body}
 
-        # 测试每个参数被删除的场景
-        for key in original_params.keys():
-            params = original_params.copy()
-            params.pop(key, None)
-            print("测试场景：验证参数必填项")
-            send_request(request_method, url, params, headers)
-
-        # 测试整数类型参数的边界值
-        for key, value in original_params.items():
-            if isinstance(value, int):
+            # 测试每个参数被留空的场景
+            for key in original_params.keys():
                 params = original_params.copy()
-                params[key] = 999999999
-                print("测试场景：验证业务规则")
+                params[key] = None if isinstance(params[key], int) else ''
+                print("测试场景：验证参数值必填项")
+                send_request(request_method, base_url, params, headers)
+
+            # 测试每个参数被删除的场景
+            for key in original_params.keys():
+                params = original_params.copy()
+                params.pop(key, None)
+                print("测试场景：验证参数必填项")
+                send_request(request_method, base_url, params, headers)
+
+            # 测试整数类型参数的边界值
+            for key, value in original_params.items():
+                if isinstance(value, int):
+                    params = original_params.copy()
+                    params[key] = 999999999
+                    print("测试场景：验证业务规则")
+                    send_request(request_method, base_url, params, headers)
+
+            # 测试 Authorization 或 token 头部为空的场景
+            if 'Authorization' in headers or 'token' in headers:
+                headers_copy = headers.copy()
+                if 'Authorization' in headers_copy:
+                    headers_copy['Authorization'] = ''
+                if 'token' in headers_copy:
+                    headers_copy['token'] = ''
+                print("测试场景：验证鉴权判定")
+                send_request(request_method, base_url, original_params, headers_copy)
+
+            # 测试时间盲注的场景
+            params = original_params.copy()
+            last_key = list(original_params.keys())[-1]  # 获取最后一个参数的键
+            # 检查参数类型是否是字符串或整数，如果是列表则跳过测试
+            if not isinstance(params[last_key], list):
+                if isinstance(params[last_key], int):
+                    params[last_key] = f"{original_params[last_key]} OR IF(1=1, SLEEP(5), 0)"  # 修改最后一个参数的值
+                elif isinstance(params[last_key], str):
+                    params[last_key] = original_params[last_key] + " OR IF(1=1, SLEEP(5), 0)"
+
+                print("测试场景：验证SQL盲注(时间盲注)")
+                send_request(request_method, base_url, params, headers)
+
+        else:
+            # 提取并解析请求体
+            request_body_start = file_content.index("{")
+            request_body_end = file_content.index("\nHTTP/1.1 200")  # 假设响应始终以"HTTP/1.1 200"开始
+            request_body = file_content[request_body_start:request_body_end].strip()
+            original_params = json.loads(request_body)
+
+            # 测试每个参数被留空的场景
+            for key in original_params.keys():
+                params = original_params.copy()
+                params[key] = None if isinstance(params[key], int) else ''
+                print("测试场景：验证参数值必填项")
                 send_request(request_method, url, params, headers)
 
-        # 测试 Authorization 或 token 头部为空的场景
-        if 'Authorization' in headers or 'token' in headers:
-            headers_copy = headers.copy()
-            if 'Authorization' in headers_copy:
-                headers_copy['Authorization'] = ''
-            if 'token' in headers_copy:
-                headers_copy['token'] = ''
-            print("测试场景：验证鉴权判定")
-            send_request(request_method, url, original_params, headers_copy)
+            # 测试每个参数被删除的场景
+            for key in original_params.keys():
+                params = original_params.copy()
+                params.pop(key, None)
+                print("测试场景：验证参数必填项")
+                send_request(request_method, url, params, headers)
 
-        # 测试时间盲注的场景
-        params = original_params.copy()
-        last_key = list(original_params.keys())[-1]  # 获取最后一个参数的键
-        # 检查参数类型是否是字符串或整数，如果是列表则跳过测试
-        if not isinstance(params[last_key], list):
-            if isinstance(params[last_key], int):
-                params[last_key] = f"{original_params[last_key]} OR IF(1=1, SLEEP(5), 0)"  # 修改最后一个参数的值
-            elif isinstance(params[last_key], str):
-                params[last_key] = original_params[last_key] + " OR IF(1=1, SLEEP(5), 0)"
+            # 测试整数类型参数的边界值
+            for key, value in original_params.items():
+                if isinstance(value, int):
+                    params = original_params.copy()
+                    params[key] = 999999999
+                    print("测试场景：验证业务规则")
+                    send_request(request_method, url, params, headers)
 
-            print("测试场景：验证SQL盲注(时间盲注)")
-            send_request(request_method, url, params, headers)
+            # 测试 Authorization 或 token 头部为空的场景
+            if 'Authorization' in headers or 'token' in headers:
+                headers_copy = headers.copy()
+                if 'Authorization' in headers_copy:
+                    headers_copy['Authorization'] = ''
+                if 'token' in headers_copy:
+                    headers_copy['token'] = ''
+                print("测试场景：验证鉴权判定")
+                send_request(request_method, url, original_params, headers_copy)
+
+            # 测试时间盲注的场景
+            params = original_params.copy()
+            last_key = list(original_params.keys())[-1]  # 获取最后一个参数的键
+            # 检查参数类型是否是字符串或整数，如果是列表则跳过测试
+            if not isinstance(params[last_key], list):
+                if isinstance(params[last_key], int):
+                    params[last_key] = f"{original_params[last_key]} OR IF(1=1, SLEEP(5), 0)"  # 修改最后一个参数的值
+                elif isinstance(params[last_key], str):
+                    params[last_key] = original_params[last_key] + " OR IF(1=1, SLEEP(5), 0)"
+
+                print("测试场景：验证SQL盲注(时间盲注)")
+                send_request(request_method, url, params, headers)
 
 
 def send_request(request_method, url, params, headers):
@@ -89,4 +149,3 @@ def send_request(request_method, url, params, headers):
 file_paths = glob.glob('./apitext_post_json/*.txt')
 for file_path in file_paths:
     extract_request_details_and_send(file_path)
-
